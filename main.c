89c9b56a5edb4fab3563293b81423c60f38e9834
andre-axt <andreaxtbachtold@gmail.com>
2025-07-14 22:10:29 -0300

    edit

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fileutils.h";
 
 int main()
 {
-    void cleanBuffer();
-    char readFile();
-    void writeFile();
-    void alterFile();
-    char* writeFileName();
-    
     printf("Do you want read[1], write[2] or  alter[3] the file csv? :)");
     int answ;
     scanf("%i", &answ);
@@ -39,89 +34,5 @@ int main()
 void cleanBuffer(){
     char c;
     while ((c = getchar()) != '\n' && c != EOF);
-}
-
-char readFile(char *fileName){
-    FILE *fptr = fopen(fileName, "r");
-    
-    if(fptr == NULL){
-        perror("open file error");
-        exit(EXIT_FAILURE);
-    }
-    
-    char *file_read[300];
-    while(fgets(file_read, sizeof(file_read), fptr) != NULL) {
-        printf("%s", file_read);
-    }
-
-    return file_read;
-}
-
-void writeFile(char *fileName){
-    FILE *file = fopen(fileName, "w");
-    
-    if(file == NULL){
-        printf("open file error");
-        return 0;
-    }
-    
-    char x[500];
-    printf("Write: \n");
-    fgets(x, sizeof(x), stdin);
-
-
-    return 0;
-}
-
-void alterFile(char *fileName){
-    FILE *file = fopen(fileName, "a");
-    
-    if(file == NULL){
-        printf("open file error");
-        return 0;
-    }
-    
-    char x[500];
-    printf("Write: \n");
-    fgets(x, sizeof(x), stdin);
-
-
-    return 0;
-    
-}
-
-char* writeFileName(){
-
-    unsigned int strpspn();
-
-    char *x;
-    int size = 30;
-    
-    x = malloc(size * sizeof(char));
-    
-    if (x == NULL){
-        fprintf(stderr, "memory allocation error");
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("Write file name:\n");
-    fgets(x, size, stdin);
-    x[strpspn(x, "\n")] = '\0';
-
-    
-    return x;
-}
-
-unsigned int strpspn(const char *x, const char *y){
-    unsigned int a, b;
-
-    for(a = 0; x[a] != '\0'; a++){
-        for(b = 0; y[b] != '\0'; b++){
-            if(x[a] == y[b]){
-                return a;
-            }
-        }
-    }
-    return a;
 }
 
